3765b28b236e15589521992b877bb54812eac7f6
sergienkoyura <sergienkoyura5@gmail.com>
2025-04-29 16:34:18 +0300

    Initial Commit

---
+++ wanderly-frontend/wanderly_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  wanderly_frontendApp.swift
+//  wanderly-frontend
+//
+//  Created by Yurii Serhiienko on 29.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct wanderly_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
